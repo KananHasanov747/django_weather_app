81193bd2feae303af0e23c9df497d56b87673e13
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-02-21 06:09:46 +0400

    fix(server.py): update the code

--- server.py
+++ server.py
@@ -3,14 +3,16 @@ import environ
 
 from django.conf import settings
 
-env = environ.Env()
+env = environ.Env(DJANGO_PORT=(int, 443))
 
 if __name__ == "__main__":
     uvicorn.run(
         "config.asgi:application",
         host="0.0.0.0",  # Bind to all interfaces
-        port=443,
+        port=env("DJANGO_PORT"),
         reload=settings.DEBUG,  # Auto-reload in development
+        reload_dirs=[settings.BASE_DIR],
+        reload_includes=["*.py", "*.html", "*.js", "*.css"],
         workers=4,  # Number of worker processes
         ssl_keyfile=env("DJANGO_SSL_KEYFILE"),
         ssl_certfile=env("DJANGO_SSL_CERTFILE"),
