6fec8535530a89267e6006041d3163205460fbe4
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-02-22 18:26:13 +0400

    refactor(server.py): add **kwargs and `if` for reload and workers

--- server.py
+++ server.py
@@ -6,15 +6,23 @@ from django.conf import settings
 env = environ.Env(DJANGO_PORT=(int, 443))
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "config.asgi:application",
-        host="0.0.0.0",  # Bind to all interfaces
-        port=env("DJANGO_PORT"),
-        reload=settings.DEBUG,  # Auto-reload in development
-        reload_dirs=[settings.BASE_DIR],
-        reload_includes=["*.py", "*.html", "*.js", "*.css"],
-        workers=4,  # Number of worker processes
-        ssl_keyfile=env("DJANGO_SSL_KEYFILE"),
-        ssl_certfile=env("DJANGO_SSL_CERTFILE"),
-        timeout_keep_alive=30,
-    )
+    kwargs = {
+        "host": "0.0.0.0",  # Bind to all interfaces
+        "port": env("DJANGO_PORT"),
+        **(
+            {
+                "reload": True,  # Auto-reload in development
+                "reload_dirs": [settings.BASE_DIR],
+                "reload_includes": ["*.py", "*.html", "*.js", "*.css"],
+            }
+            if settings.DEBUG
+            else {
+                "workers": 4,  # Number of worker processes
+            }
+        ),
+        "ssl_keyfile": env("DJANGO_SSL_KEYFILE"),
+        "ssl_certfile": env("DJANGO_SSL_CERTFILE"),
+        "timeout_keep_alive": 30,
+    }
+
+    uvicorn.run("config.asgi:application", **kwargs)
