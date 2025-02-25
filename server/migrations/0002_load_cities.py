8f57ffb71c09ce58b61cd44fe0e7da0043f2d138
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-02-25 21:14:12 +0400

    refactor(server.migrations): add RunSQL support for Postgres

--- server/migrations/0002_load_cities.py
+++ server/migrations/0002_load_cities.py
@@ -2,32 +2,30 @@ import os
 from django.db import migrations
 from django.conf import settings
 
+import environ
 
-def load_cities(apps, schema_editor):
+env = environ.Env(DJANGO_POSTGRES=(bool, False))
+
+
+def load_sql():
     file_path = os.path.join(settings.BASE_DIR, "weather_cities_dump.sql")
-
     with open(file_path, "r", encoding="utf-8") as f:
-        sql_content = f.read()
+        return f.read()
 
+
+def load_cities_sqlite(apps, schema_editor):
+    sql_content = load_sql()
     cursor = schema_editor.connection.cursor()
 
-    if schema_editor.connection.vendor == "sqlite":
-        # set PRAGMA settings to speed up the loading
-        cursor.execute("PRAGMA synchronous=OFF;")
-        cursor.execute("PRAGMA cache_size=10000;")
-        cursor.execute("PRAGMA journal_mode=MEMORY;")
+    # set PRAGMA settings to speed up the loading
+    cursor.execute("PRAGMA synchronous=OFF;")
+    cursor.execute("PRAGMA cache_size=10000;")
+    cursor.execute("PRAGMA journal_mode=MEMORY;")
 
-        cursor.executescript(sql_content)
-
-    else:  # postgres & others
-        statements = sql_content.split(";")
-        for statement in statements:
-            statement = statement.strip()
-            if statement:
-                cursor.execute(statement)
+    cursor.executescript(sql_content)
 
 
-def unload_cities(apps, schema_editor):
+def unload_cities_sqlite(apps, schema_editor):
     # reverse operation: delete all rows from the City table
     City = apps.get_model("server", "City")
     City.objects.all().delete()
@@ -38,6 +36,15 @@ class Migration(migrations.Migration):
 
     dependencies = [("server", "0001_initial")]
 
-    operations = [
-        migrations.RunPython(load_cities, reverse_code=unload_cities),
-    ]
+    if env("DJANGO_POSTGRES"):
+        sql_statements = load_sql()
+        operations = [
+            migrations.RunSQL(
+                sql_statements, reverse_sql="DELETE FROM weather_cities;"
+            ),
+        ]
+
+    else:
+        operations = [
+            migrations.RunPython(load_cities_sqlite, reverse_code=unload_cities_sqlite),
+        ]
