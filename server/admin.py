e78c9b0d81367b164bfa42016349cf933a5c91d2
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-02-27 01:03:35 +0400

    chore: add CityAdmin for admin panel

--- server/admin.py
+++ server/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import City
 
-# Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ["city", "country", "population"]
+    ordering = ["city"]
+
+
+admin.site.register(City, CityAdmin)
