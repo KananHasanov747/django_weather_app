c1f4fa83b1c7080bef10b1937e2673a5ffec48cd
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-03-10 21:53:48 +0400

    refactor(server.py, manage.py): update variable handling

--- manage.py
+++ manage.py
@@ -7,23 +7,6 @@ import environ
 
 def main():
     """Run administrative tasks."""
-    # Set ENV_NAME based on the command
-    if len(sys.argv) > 1:
-        command = sys.argv[1]
-        if command in ("pytest", "check"):
-            os.environ["DJANGO_ENVIRONMENT"] = "production"
-            os.environ["DJANGO_ENV_NAME"] = ".env.prod"
-
-        elif command == "preprod":
-            os.environ["DJANGO_ENVIRONMENT"] = "staging"
-            os.environ["DJANGO_ENV_NAME"] = ".env.staging"
-        else:  # for development
-            os.environ["DJANGO_ENVIRONMENT"] = "development"
-            os.environ["DJANGO_ENV_NAME"] = ".env.dev"
-    else:
-        os.environ["DJANGO_ENVIRONMENT"] = "development"
-        os.environ["DJANGO_ENV_NAME"] = ".env.dev"
-
     env = environ.Env()
     environ.Env.read_env(env("DJANGO_ENV_NAME"))
     os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
--- server.py
+++ server.py
@@ -6,12 +6,10 @@ if __name__ == "__main__":
     if "DJANGO_ENVIRONMENT" not in os.environ:
         raise ValueError("DJANGO_ENVIRONMENT must be set")
 
-    if "DJANGO_ENV_NAME" not in os.environ:
-        environ.Env.read_env(".env.prod")
-
     env = environ.Env(
         DJANGO_PORT=(int, 443), DJANGO_HOST=(str, "0.0.0.0"), DJANGO_SSL=(bool, False)
     )
+    environ.Env.read_env(env("DJANGO_ENV_NAME"))
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     from django.conf import settings
