dde88508111d91f10d15d0624511a5946f98c329
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-03-13 01:56:05 +0400

    chore(server/urls): remove DJANGO_API_URLS

--- server/urls.py
+++ server/urls.py
@@ -1,14 +1,9 @@
-import os
-from django.conf import settings
-
 from django.urls import path
 
 from server import views
 
 
 urlpatterns = [
-    path(
-        "api/" if settings.DEBUG else f"{os.getenv("DJANGO_API_URL")}/", views.api.urls
-    ),
+    path("api/", views.api.urls),
     path("search/<str:query>/", views.city_search_view, name="city_search"),
 ]
