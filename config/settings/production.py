11b6c2cbede514747649aa247125427098ec8483
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-03-21 23:44:15 +0400

    chore: add ',' in split() to separate string with commas

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ from ._base import *  # noqa
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = env("DJANGO_CSRF_TRUSTED_HOSTS").split()
+CSRF_TRUSTED_ORIGINS = env("DJANGO_CSRF_TRUSTED_HOSTS").split(",")  # noqa
 # Uncomment if using http protocol in production
 # CSRF_COOKIE_SECURE = False
 # CSRF_COOKIE_HTTPONLY = False
