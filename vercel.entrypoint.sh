fc8311de4198a47d2b7ea454729bbb55e639d9a8
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-03-28 16:53:29 +0400

    chore: update vercel.entrypoint.sh

--- vercel.entrypoint.sh
+++ vercel.entrypoint.sh
@@ -3,6 +3,7 @@
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONUNBUFFERED=1
 
+python3 manage.py migrate zero
 python3 manage.py migrate
 python3 manage.py createsuperuser --no-input
 python3 -m gunicorn config.asgi:app
