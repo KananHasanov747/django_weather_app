0abd59600e026c71135142b80ca56493049484b0
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-03-28 17:09:04 +0400

    chore: update vercel config

--- vercel.build_files.sh
+++ vercel.build_files.sh
@@ -5,3 +5,5 @@ export PYTHONUNBUFFERED=1
 
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
+python3 manage.py migrate
+python3 manage.py createsuperuser --no-input
--- vercel.entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PYTHONDONTWRITEBYTECODE=1
-export PYTHONUNBUFFERED=1
-
-python3 manage.py migrate zero
-python3 manage.py migrate
-python3 manage.py createsuperuser --no-input
-python3 -m gunicorn config.asgi:app
