c77fb886323c4bfd0eadee96d9a5d29e1a57210a
KananHasanov747 <83110215+KananHasanov747@users.noreply.github.com>
2025-04-01 00:56:18 +0400

    build: comment line 'createsuperuser' to avoid repeat

--- vercel.build_files.sh
+++ vercel.build_files.sh
@@ -6,4 +6,4 @@ export PYTHONUNBUFFERED=1
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input --clear
 python3 manage.py migrate
-python3 manage.py createsuperuser --no-input
+# python3 manage.py createsuperuser --no-input
